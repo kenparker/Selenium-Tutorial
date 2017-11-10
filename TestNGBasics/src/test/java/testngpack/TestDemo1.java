d016360f8adf8c0fd4594787c37bc9d7264de231
Angelo <Angelo@DESKTOP-PQRPD3V.fritz.box>
2017-11-10 12:35:07 +0100

    TestNG with testng.xml

---
+++ TestNGBasics/src/test/java/testngpack/TestDemo1.java
@@ -0,0 +1,11 @@
+package testngpack;
+
+import org.testng.annotations.Test;
+
+public class TestDemo1 {
+    
+    @Test
+    public void doSomething() {
+        System.out.println("something from Demo1");
+    }
+}
