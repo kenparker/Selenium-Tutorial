043899a809a8c466737630eb93a6e3942767febd
angelo <magangde@yahoo.de>
2017-11-29 14:19:58 +0100

    Testcases updated

--- LoadTestDemo1/src/test/java/com/angelo/loadtestdemo1/TestLogin.java
+++ LoadTestDemo1/src/test/java/com/angelo/loadtestdemo1/TestLogin.java
@@ -1,25 +1,47 @@
 package com.angelo.loadtestdemo1;
 
+import java.io.IOException;
+
+import static org.testng.Assert.fail;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class TestLogin extends LoginToCampusManagement{
+public class TestLogin extends LoginToCampusManagement {
 
     @BeforeClass
     public static void before() {
-        
     }
-    
-    @Test(invocationCount = 5, threadPoolSize = 2)
+
+    @Test(invocationCount = 10, threadPoolSize = 5, enabled = true)
     public static void testLogin() {
-        LoginToCampusManagement loginToCampusManagement = new LoginToCampusManagement();
-       loginToCampusManagement.loginManagement();
-        loginToCampusManagement.driver.close();
+        testOneLoginLocal();
     }
-    
-        
+
+    @Test(enabled = false)
+    public static void testOneLoginLocal() {
+        LoginToCampusManagement login = new LoginToCampusManagement();
+        login.setWebdriverpropertyfile("src/main/java/com/angelo/properties/WebDriverAttributes.properties");
+        login(login);
+    }
+
+    @Test(enabled = false)
+    public static void testOneLoginRemote() {
+        LoginToCampusManagement login = new LoginToCampusManagement();
+        login.setWebdriverpropertyfile("src/main/java/com/angelo/properties/WebDriverAttributes1.properties");
+        login(login);
+    }
+
+    private static void login(LoginToCampusManagement login) {
+        try {
+            login.loginManagement();
+            login.driver.quit();
+        } catch (IOException ex) {
+            fail();
+        }
+    }
+
     @AfterClass
     public static void end() {
     }
---
+++ LoadTestDemo1/src/test/java/com/angelo/loadtestdemo1/TestWebDriverManagement.java
@@ -0,0 +1,42 @@
+package com.angelo.loadtestdemo1;
+
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class TestWebDriverManagement {
+
+    WebDriverManagement webDriverManagement = new WebDriverManagement();
+
+    @Test
+    public void testLocalWebDriver() throws IOException {
+        webDriverManagement.build("src/main/java/com/angelo/properties/WebDriverAttributes.properties");
+        WebDriver webDriver = webDriverManagement.getWebDriver();
+        String s = webDriver.getClass().toString();
+        assertEquals("class org.openqa.selenium.chrome.ChromeDriver", s);
+        webDriver.close();
+    }
+
+    @Test
+    public void testRemoteWebDriver() throws IOException {
+        webDriverManagement.build("src/main/java/com/angelo/properties/WebDriverAttributes1.properties");
+        WebDriver webDriver = webDriverManagement.getWebDriver();
+        String s = webDriver.getClass().toString();
+        assertEquals("class org.openqa.selenium.remote.RemoteWebDriver", s);
+        webDriver.quit();
+    }
+
+    @Test
+    public void testPropertyFileIsWrong()  {
+        try {
+            webDriverManagement.build("src/main/java/com/angelo/properties/blablabal.properties");
+            fail();
+        } catch (IOException e) {
+            // ok
+        }
+
+    }
+}
