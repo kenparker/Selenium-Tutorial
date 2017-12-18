20f8e03768149fc14ee98950d4a3d81f91f6c978
angelo <magangde@yahoo.de>
2017-12-18 09:16:25 +0100

    refactoring TestNgBasicWithTestngxml

---
+++ TestNgBasicWithTestngxml/src/test/java/testngpack/TestDemo2.java
@@ -0,0 +1,13 @@
+package testngpack;
+
+import org.testng.annotations.Test;
+
+public class TestDemo2 {
+ 
+    
+    @Test
+    public void test1() {
+        System.out.println("I'm from Demo2");
+    }
+    
+}
---
+++ TestNgBasicWithTestngxml/src/test/java/testngpack/TestSuite1.java
@@ -0,0 +1,11 @@
+package testngpack;
+
+import org.testng.annotations.BeforeTest;
+
+public class TestSuite1 {
+
+    @BeforeTest
+    public void suite1() {
+        System.out.println("suite 1");
+    }
+}
