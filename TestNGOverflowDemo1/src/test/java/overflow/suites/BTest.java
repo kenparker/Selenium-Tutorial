15e0453aa0d89c14007f08110ebb485b5f993006
angelo <magangde@yahoo.de>
2017-12-19 14:54:06 +0100

    Demo to understand @BeforeTest and @BeforeClass

---
+++ TestNGOverflowDemo1/src/test/java/overflow/suites/BTest.java
@@ -0,0 +1,11 @@
+package overflow.suites;
+
+import org.testng.annotations.Test;
+
+public class BTest extends Base {
+
+    @Test
+    public void test2() {
+        System.out.println("test2 : " + driver.getClass());
+    }
+}
